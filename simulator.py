1c777b98299493a56c5c16930a08841ba8d7d79e
6x9equals42 <6x9equals42@users.noreply.github.com>
2018-04-02 22:23:01 -0800

    initial commit

---
+++ simulator.py
@@ -0,0 +1,165 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import time
+
+from sympy import symbols
+from sympy.physics import mechanics
+
+from sympy import Dummy, lambdify
+from scipy.integrate import odeint
+
+from matplotlib import animation
+
+
+def integrate_pendulum(n, times,
+                       initial_positions=135,
+                       initial_velocities=0,
+                       lengths=None, masses=1):
+    """Integrate a multi-pendulum with `n` sections"""
+    #-------------------------------------------------
+    # Step 1: construct the pendulum model
+    print("generating differential equations")
+    # Generalized coordinates and velocities
+    # (in this case, angular positions & velocities of each mass)
+    q = mechanics.dynamicsymbols('q:{0}'.format(n))
+    u = mechanics.dynamicsymbols('u:{0}'.format(n))
+
+    # mass and length
+    m = symbols('m:{0}'.format(n))
+    l = symbols('l:{0}'.format(n))
+
+    # gravity and time symbols
+    b, g, t = symbols('b,g,t')
+
+    #--------------------------------------------------
+    # Step 2: build the model using Kane's Method
+
+    # Create pivot point reference frame
+    A = mechanics.ReferenceFrame('A')
+    P = mechanics.Point('P')
+    P.set_vel(A, 0)
+
+    # lists to hold particles, forces, and kinetic ODEs
+    # for each pendulum in the chain
+    particles = []
+    forces = []
+    kinetic_odes = []
+
+    for i in range(n):
+        # Create a reference frame following the i^th mass
+        Ai = A.orientnew('A' + str(i), 'Axis', [q[i], A.z])
+        Ai.set_ang_vel(A, u[i] * A.z)
+
+        # Create a point in this reference frame
+        Pi = P.locatenew('P' + str(i), l[i] * Ai.x)
+        Pi.v2pt_theory(P, A, Ai)
+
+        # Create a new particle of mass m[i] at this point
+        Pai = mechanics.Particle('Pa' + str(i), Pi, m[i])
+        particles.append(Pai)
+
+        # Set forces & compute kinematic ODE
+        forces.append((Pi, m[i] * g * A.x - b * u[i] / l[i] * Ai.y))
+        kinetic_odes.append(q[i].diff(t) - u[i])
+
+        P = Pi
+
+    # Generate equations of motion
+    KM = mechanics.KanesMethod(A, q_ind=q, u_ind=u,
+                               kd_eqs=kinetic_odes)
+    fr, fr_star = KM.kanes_equations(forces, particles)
+
+    #-----------------------------------------------------
+    # Step 3: numerically evaluate equations and integrate
+
+    # initial positions and velocities – assumed to be given in degrees
+    y0 = np.deg2rad(np.concatenate([np.broadcast_to(initial_positions, n),
+                                    np.broadcast_to(initial_velocities, n)]))
+
+    # lengths and masses
+    if lengths is None:
+        lengths = np.ones(n) / n
+    lengths = np.broadcast_to(lengths, n)
+    masses = np.broadcast_to(masses, n)
+
+    # Fixed parameters: gravitational constant, lengths, and masses
+    parameters = [b] + [g] + list(l) + list(m)
+    parameter_vals = [-0.09] + [9.81] + list(lengths) + list(masses)
+
+    # define symbols for unknown parameters
+    unknowns = [Dummy() for i in q + u]
+    unknown_dict = dict(zip(q + u, unknowns))
+    kds = KM.kindiffdict()
+
+    # substitute unknown symbols for qdot terms
+    mm_sym = KM.mass_matrix_full.subs(kds).subs(unknown_dict)
+    fo_sym = KM.forcing_full.subs(kds).subs(unknown_dict)
+
+    # create functions for numerical calculation
+    mm_func = lambdify(unknowns + parameters, mm_sym)
+    fo_func = lambdify(unknowns + parameters, fo_sym)
+    print("generating derivatives of parameters")
+    # function which computes the derivatives of parameters
+    def gradient(y, t, args):
+        vals = np.concatenate((y, args))
+        sol = np.linalg.solve(mm_func(*vals), fo_func(*vals))
+        return np.array(sol).T[0]
+
+    print("integrating")
+    # ODE integration
+    return odeint(gradient, y0, times, args=(parameter_vals,))
+
+def get_xy_coords(p, lengths=None):
+    """Get (x, y) coordinates from generalized coordinates p"""
+    p = np.atleast_2d(p)
+    n = p.shape[1] // 2
+    if lengths is None:
+        lengths = np.ones(n) / n
+    zeros = np.zeros(p.shape[0])[:, None]
+    x = np.hstack([zeros, lengths * np.sin(p[:, :n])])
+    y = np.hstack([zeros, -lengths * np.cos(p[:, :n])])
+    return np.cumsum(x, 1), np.cumsum(y, 1)
+
+def animate_pendulum(n):
+    t = np.linspace(0, 10, 200)
+    p = integrate_pendulum(n, t, masses = 1)
+    x, y = get_xy_coords(p)
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    ax.axis('off')
+    ax.set(xlim=(-1.1, 1.1), ylim=(-1.1, 1.1))
+
+    line, = ax.plot([], [], 'o-', lw=2)
+
+    def init():
+        line.set_data([], [])
+        return line,
+
+    def animate(i):
+        line.set_data(x[i], y[i])
+        return line,
+
+    anim = animation.FuncAnimation(fig, animate, frames=len(t),
+                                   interval=1000 * t.max() / len(t),
+                                   blit=True, init_func=init)
+    plt.close(fig)
+    return anim
+start = time.time()
+
+# t = np.linspace(0, 10, 200)
+# p = integrate_pendulum(7, t, masses = 1)
+anim = animate_pendulum(7)
+
+
+
+#Set up formatting for the movie files
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=20, metadata=dict(artist='Me'), bitrate=1800)
+
+
+anim.save('testnegfriction.mp4', writer=writer)
+
+end = time.time()
+print(end - start)
